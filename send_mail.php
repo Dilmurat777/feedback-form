62ba85a1e77e6852d3289aa9543cd99919f2c734
Dilmurat777 <mrdilmurat777@gmail.com>
2023-11-15 13:50:14 +0600

    fixed

--- send_mail.php
+++ send_mail.php
@@ -4,16 +4,16 @@
 	use PHPMailer\PHPMailer\Exception;
 
 
-	require 'PHPMailer/src/PHPMailer.php';
-	require 'PHPMailer/src/Exception.php';
+	require "PHPMailer/src/PHPMailer.php";
+	require "PHPMailer/src/Exception.php";
 
 	$mail = new PHPMailer(true);
-	$mail -> CharSet = 'UFT-8';
+	$mail->CharSet = "UFT-8";
 
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$phone = $_POST['phone'];
-	$message = $_POST['message'];
+	$name = $_POST["name"];
+	$email = $_POST["email"];
+	$phone = $_POST["phone"];
+	$message = $_POST["message"];
 
 	$body = $name . ' ' . $email . ' ' . $phone . ' ' . $message;
 
